d84612e772bcafed1d3b2c6b8eeca5a0706fb770
Becky Fox <foxy2530@gmail.com>
2022-06-27 17:26:01 +0100

    added python_notes folder and made a recap file for notes

---
+++ python_notes/recap.py
@@ -0,0 +1,20 @@
+# use comments to make notes about your code for yourself and other developers
+# use a hash key at the start of the line for the comment
+# you can select the line and use the ctrl and forward slash key to make the line a comment
+
+print('this is my file')
+
+# variables in this example is greeting, = means it assign the variable to the string and the string is ("hello world")
+greeting = "Hello World"
+
+print(greeting) #(greeting) will print out the assigned string in this example it will print out Hello World in the terminal
+
+print("this is a string to displaying characters") # this is a string
+print("536437") # this is still a string because of the quotes
+print(12358) # this is an integer because it doesn't have quotes
+print(36+8) # this is an example of an calculation 
+print(2.3) # floating point
+#Boolan (True or False) first letter of true or false needs to be capitial letter
+print(True) # Boolan
+print(False) # Boolan
+print(None) # - blank / null data (place holder)
