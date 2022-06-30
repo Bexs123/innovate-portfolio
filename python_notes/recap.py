0512fe07e4cd0532d58d7e6d79adde0ebf107710
Becky Fox <foxy2530@gmail.com>
2022-06-30 04:08:55 +0100

    Modified and addded more notes into the recap.py file

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,6 +1,7 @@
 import random #random is a library and it needs to be imported at the top of the code, because code reads and execuates from top to bottom
 
 # use comments to make notes about your code for yourself and for other developers
+# Comments can be on multiple lines
 # use a hash key at the start of the line for the comment
 # you can select the line(s) and use the ctrl and forward slash key to make the line(s) a comment
 
@@ -11,7 +12,7 @@ greeting = "Hello World"
 
 print(greeting) #(greeting) will print out the assigned string in this example it will print out Hello World in the terminal
 
-print("this is a string to displaying characters") # this is a string, because it is an characters within quotes 
+print("this is a string to displaying characters") # this is a string, because the characters are within quotes 
 print("536437") # this is still a string because of the quotes
 print(12358) # this is an integer because it doesn't have quotes - integer are whole numbers
 print(2.3) # floating point
@@ -27,6 +28,14 @@ print(5*3)#Example of a multiplication - Answer will be 15
 print(2**2)#Example of power too or exponentiation Operator - Answer will be 4
 print(15/3)#Example of division - Answer will be 5
 print(13%1) #Example of a modulus% - Answer will  be 0 - Modulus shows the remainder of a division
+
+# Order of Operation PEMDAS:
+# P - Parenthese
+# E - Exponent
+# M - Multiplication
+# D - Division
+# A - Addition
+# S - Subtraction
 
 #Assignment operators to store values (=, *=, +=, /=, -=)
 
@@ -47,16 +56,16 @@ print("HELLO".lower()) #changes upper case to lower case
 
 print("hello EVERYONE. THIS is innovate".capitalize()) # Changes the first character to capital - also it changes any upper case to lower case
 
-print("This quick brown fox".count("o"))#counts o - Answer is 2
+#The count() method returns the number of elements with the specified value
+print("The quick brown fox".count("o"))#counts o - Answer is 2
+print("The quick brown fox fox fox".count("fox"))#counts fox - Answer is 3
+print("The quick brown fox  fox  fox".count("fox  "))#counts fox - Answer is 2 because of the amount of whitespace inbetween each fox
 
-print("This quick brown fox fox fox".count("fox"))#counts fox - Answer is 3
-print("This quick brown fox  fox  fox".count("fox  "))#counts fox - Answer is 2 because of the amount of whitespace inbetween each fox
-
-print("This quick brown fox".find("fox"))#find() in this example will find the word fox and the - Answer is 17
-#find() method finds the first occurrence of the specified value.
+print("TheT quick brown fox".find("T"))#Answer is Index 0 because it is the first occurrence of the specified value
+print("The quick brown fox".find("fox"))#find() in this example will find the word fox and the - Answer is 16 (inculdeds whitespace)
 
 #Example of the find() method returing -1, it returns -1 because the value is not found.
-print("This quick brown fox".find("dog"))#Answer is -1
+print("The quick brown fox".find("dog"))#Answer is -1
 
 print("The quick brown fox".replace("fox", "frog"))#In this example it will replace all the fox words to frog.
 
@@ -70,48 +79,84 @@ print(random.randint(1, 10)) #Generates a random integer between 1 and 10, inclu
 
 #random is a library in python and you need to import them if you want to use them.
 
-# can use use from random import random, randint, uniform
+# another way of importing the libraries is:-
+# from random import random, randint, uniform
 
-my_name = ""
-my_age = ""
+#Variables
+#In everyday life we can store items in boxes to retrieve later.
+#You can store all different types of items can be stored in the box at different times.
+#In coding you can give variables names so we can access things inside them.
+
+my_name = "Boris"
+my_age = 21
 student = False
 
 print(my_name, my_age, student)
 
-print("Hello my name is ")
-#concatenate (concatenating)
+#concatenate
+print("Hello my name is", my_name)
+print("I am", my_age) #Answer I am 21
 
-#.format()  method allows to use placeholder {}
-# {} are for placeholders 
+print("Hello my name is " + my_name)
+print("I am" + my_age) # Answer - TypeError: can only concatenate str (not "int") to str
+#All the error means is that you can only concatenate or link strings together, not integer to strings
+
+#Concatenation only works on string data types
+
+#Legacy code, all this means that it is outdated, however it can still be used.
+#{} are used for placeholders, it just means that you can add data later.
+# .format()
+
 print("Hello my name is {} and I am {} years old".format(my_name, my_age))
+#Place the variables into the brackets after .format, the variables have to go in the correct order of the placeholders.
 
-#f strings, new best practise
+#f strings, new best practise, f strings are much cleaner to write and read then using .format.
 print(f"Hello my name is {my_name} and I am {my_age} years old")
 
-balance=100
-amount=20
+balance = 450
+amount = 45
 
-balance=amount+balance
-balance +=amount
+balance = amount + balance
+balance += amount
+
+print(balance)
+
+# Input() - allows whatever a user types into the terminal to be saved to a variable - in this case response
+
+input("What is your name?") # Saved in the random memory of the computer
+
+answer = input("What is your name?") # Save as an variable, whatever the user types will be saved to the variable answer.
 
 #\n is an ecaspe character \n will print the answer in a newline
-answer = input("What are you going to do today \n?")
+answer = input("What are you going to do today? \n")
 print(answer)
 
 #if else
 music = "classical"
 
-if music == "classical": #comparsion Operators
+if music == "classical": # == is a comparison operators
     print("Oh no! It's classical music again.")
 elif music == "no music":
     print("Ahh, peace and quiet")
-else: #doen't need a comparsion operator 
+else: # doesn't need comparison operator
     print("Nice and noisy.")
     
-# comparsion is checking the if statements are True or Falese
+# comparison operators checks the if else statements are True or False
 
-num=90
-num2=290
+# == Equal - this operator checks if the value on the left of the operator is equal to the one on the right
+
+# != Not equal - it checks if the value on the left of the operator is not equal to the one on the right.
+
+# Relational Operator - carries out the comparsion between operands
+
+# Greater than or equal to >= - checks if the value on the left of the operator is greater than or equal to the one on the right.
+# Greater than > - checks if the value on the left of the operator is greater than the one on the right.
+# Less than or equal to <= - check if the value on the left of the operator is lesser than or equal to the one on the right
+# Less than - checks if the value on the left of the operator is lesser than the one on the right
+
+# An example of using Greater than
+num = 90
+num2 = 290
 
 if num > num2:
     print(f"{num} is bigger")
@@ -121,6 +166,15 @@ else:
     print("Both are equal")
     
 #logical operator and (both side must be true)
+# These are conjunctions that you can use to combine more than one condition.
+# There are three logical operator:- and, or, and not.
+
+# and Operator - if the conditions on both sides of the operator are true, then the expression as a whole is true.
+
+# or Operator - The expression is false only if both the statements around the operator are false. Otherwise, it is true.
+
+# not Operator - This inverts the Boolean value of an expression. It converts True to False, and False to True
+
 place ="MCR"
 weather = "cloudy"
 
@@ -130,8 +184,8 @@ elif place == "MCR" and weather == "Rain":
     print("Obvs")
 else:
     print("wait, it isn't raning")
-    
-#logical operator or
+
+# Example of or logical operator 
 
 day = "saturday"
 
@@ -140,23 +194,35 @@ if day == "saturday" or day == "sunday":
 else:
     print("When's the weekend?")
 
-#functions
-#print and input are functions
-#def means definie the function
+# Example 2
+
+day = "sunday"
+bank_hol = True
+
+if day == "saturday" or day == "sunday" or bank_hol:
+    print("A day off")
+else:
+    print("off to Innovate we go")
+
+# Functions
+#def is the keyword for defining a function.
 
 #Example of Function
 def light_switch():
     print("who turned out the lights?")
     
-    light_switch() #call the function
+light_switch() #call the function
     
-#Example of a Function with parameters
+# Example of a Function with parameters - Parameter in this example is amount and accnum
 def cash_withdrawal (amount, accnum):
     print(f"Withdrawing {amount} from account {accnum}")
     
-    cash_withdrawal(300, 50449921)
-    
+cash_withdrawal(300, 50449921) # both values are arguments
+cash_withdrawal(20, 53428091)
+cash_withdrawal(5000, 12345678)
+
 #Lists
+
 fav_songs = [
     "Bring Me to Life - Evancesences",
     "walk on water - Milk Inc",
@@ -164,10 +230,19 @@ fav_songs = [
 ]
 
 print(fav_songs)
+print(fav_songs[2]) # It will only print out the item in the list at Index position 2
+# Answer "you stole the sun from mu heart - Manic Street Preachers" 
+print(len(fav_songs)) # Print out the number of items in the list - Answer is 3
+
+fav_songs[1] = "Left Outside Alone - Anastacia" # Replaces th list item at Index position 1 to the new list item.
+# Index 1 was "walk on Water - Milk Inc" it will become "Left outside Alone - Anastacia"
 
 # .append
+fav_songs.append("Rule the World - Take That") # Adds to the end of the list
 
 #.pop
+fav_songs.pop() # .pop will remove from the end of the list
+print(fav_songs)
 
 #For Loops
 #lists are iteratation
