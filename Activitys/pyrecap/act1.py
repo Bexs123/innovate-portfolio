c243d00e59c8815c124a3bf4bcdfd0a702eff6bb
Becky Fox <foxy2530@gmail.com>
2022-07-01 21:28:14 +0100

    made changes to act.py

--- Activitys/pyrecap/act1.py
+++ Activitys/pyrecap/act1.py
@@ -1,21 +1,25 @@
 #Create variable that holds the text "welcome to Code Nation"
-welcome = "welcome to Code Nation"
-print(welcome)
+string_1 = "welcome to Code Nation"
+print(string_1)
 
 #Find the length of the string and save this to a new variable
-print(len(welcome))
-num = (len(welcome))
-# print(welcome)
+string2_len = len(string_1)
+print(string2_len)
 
 #Create a function that checks if the string length is even
-
-
-#if it is, print the string, the length and an 
-# appropriate message in one sentence.
-
-
-#Do the same but with a different message if it's odd.
-
-
-
+def odd_even_checker(string_1):
+    string2_len = len(string_1)
+    if string2_len%2==0:
+# If it is even print the length of the string and an appropriate message
+        print(f"The length of the string {string_1} is {string2_len} and yes it even")
+    else:
+# Do the same but with a different message if it is odd
+        print(f"The length of the string {string_1} is {string2_len} it is odd")
+    
 #Change the string length so you can test all possible outcomes
+odd_even_checker("welcome 2 Code Nation")
+odd_even_checker("welcomes too Code Nationss")
+odd_even_checker("welcome too Code Nation")
+odd_even_checker("welcom t Code Ntion")
+odd_even_checker("welcome to Code Nationss")
+odd_even_checker("welcome to Code Nationssee")
