e23c444455a4429d8ba7726ea1740673f34caea5
Becky Fox <foxy2530@gmail.com>
2022-07-03 02:07:31 +0100

    updated dictionaries act3.py

--- Activitys/dictionaries/act3.py
+++ Activitys/dictionaries/act3.py
@@ -1,25 +1,52 @@
-#Create a list called fav_songs
-#Each item on the list will be a dictionary with the following keys: artist, song_name, genre, release_year
-#Recap or research to add, remove and update some of the dictionaries.
+# Create a list called fav_songs
+# Each item on the list will be a dictionary with the following keys: artist, song_name, genre, release_year
+# Recap or research to add, remove and update some of the dictionaries.
 
-fav_songs = {
+# fav_song is the variable - 
+fav_songs = [{
     "artist":"Evanescence",
     "song_name":"Bring Me To Life",
     "genre":"Metal",
     "release_year":"2003"
-}
+},
+{
+    "artist":"Manic Street Preachers",
+    "song_name":"You Stole The Sun From My Heart",
+    "genre":"Indie",
+    "release_year":"1998"
+},
+{   
+    "artist":"Savage Garden",
+    "song_name":"Crash And Burn",
+    "genre":"Pop",
+    "release_year":"1999"
+},
+{
+    "artist":"The Who",
+    "song_name":"Behind Blue Eyes",
+    "genre":"Rock",
+    "release_year":"1971"
+}]
 
-# for i in fav_songs.keys():
-#     print(i)
-    
-#Add
-fav_songs.update({"Album":"Fallen"})
 print(fav_songs)
 
-#Remove
-del fav_songs["genre"]
+fav_songs.append({ # .append method is to add a new dictionary to the current dictionaries
+    "artist":"Guns N Roses",
+    "song_name":"November Rain",
+    "genre":"Rock",
+    "release_year":"1991"
+})
+
 print(fav_songs)
 
-#update
-fav_songs.setdefault("genre", "rock")
+# Delete methods
+del (fav_songs[1])
+print(fav_songs)
+
+# Pop method delete the last one in the list unless you add the index position in the end brackets
+# fav_songs.pop()
+# fav_songs.pop(0)
+
+# Update
+fav_songs[2].update({"song_name":"Won't Get Fooled Again"})
 print(fav_songs)
