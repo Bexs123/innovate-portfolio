f7e63d170d1e423299d4b6326e7c776d8ce5f0a8
Becky Fox <foxy2530@gmail.com>
2022-07-09 21:01:25 +0100

    Just made a few changes with the wording of the notes

--- python_notes/dictionaries.py
+++ python_notes/dictionaries.py
@@ -1,16 +1,18 @@
-print('Python Dictionaries')
+print("Python Dictionaries")
+print("This is my Python Notes and Examples for Dictionaries")
 
 # They store data for us in a really specific way.
 # Just like actual dictionaries, you can look things up, however they're not in alphabetical order.
 # They are more like lists they store multiple values, we use {} to define our dictionaries.
 
 # Example of List
-my_cat = ['Salem', 'black', 'sassy']  
+my_cat = ["Salem", "black", "sassy"]
 
-# Example of Dictionary
-my_cat = {"name":"Salem",
+# Example of Dictionary - name, colour, mood are all keys, and the values are Salem, black, sassy
+my_cat = {
+          "name": "Salem", 
           "colour": "black",
-          "mood":"sassy"
+          "mood": "sassy"
           }
 
 # Unlike lists, they use key:value pairs, so you can give each element a name.
@@ -19,17 +21,17 @@ my_cat = {"name":"Salem",
 
 # My Dictionary for my pet dog
 my_dog = {
-        "name":"Lexi",
-        "colour":"Brindle",
-        "mood":"Lazy"
-          } 
+          "name": "Lexi", 
+          "colour": "Brindle", 
+          "mood": "Lazy"
+          }
 
-print(my_dog[2]) # in <module> print(my_dog[2]) KeyError: 2
+print(my_dog[2])  # in <module> print(my_dog[2]) KeyError: 2
 
 print(my_dog["name"])
 print(my_dog["colour"])
 
-print(my_dog.get('age', 'Error')) # Answer Error
+print(my_dog.get("age", "Error"))  # Answer Error
 
 # You can change a value of an existing key
 my_cat["name"] = "Whiskers"
@@ -55,9 +57,9 @@ print(list(my_dog.items()))
 # Dot notation
 # print(my_dog.keys())
 
-x =my_dog.keys()
+x = my_dog.keys()
 
-my_dog["age"]=2
+my_dog["age"] = 2
 
 print(x)
 
@@ -65,15 +67,15 @@ print(my_dog.values())
 print(my_dog.items())
 print(my_dog.get("mood"))
 
-print(my_dog.get("legs")) # causes an error
-print(my_dog.get("legs", "This key doesn't exist")) # You can add an error message
+print(my_dog.get("legs"))  # causes an error
+print(my_dog.get("legs", "This key doesn't exist"))  # You can add an error message
 print(my_dog.keys())
 print(list(my_dog.keys()))
 
-# To make it even by incorporating for loops
+# To make the output better and clearner, incorporate for loops 
 for i in my_dog.keys():
     print(i)
-    
+
 # Set a new key:value using .setdefault()
 my_dog.setdefault("hungry", True)
 
@@ -81,16 +83,14 @@ my_dog.setdefault("hungry", True)
 my_dog.setdefault("mood", "sleepy")
 
 # Update - update extising key values - add new key values
-my_dog.update({"legs":"4"})
+my_dog.update({"legs": "4"})
 print(my_dog)
 
-# remove key values by using .pop method
+# Remove keys from a dictionary using .pop()method
 my_dog.pop("mood")
 print(my_dog)
 
 # del method - lets you specify which key to delete
-my_cat = {"name": "Salem",
-          "colour": "black",
-          "mood": "sassy"}
+my_cat = {"name": "Salem", "colour": "black", "mood": "sassy"}
 del my_cat["mood"]
-print(my_cat) # deletes the key:value mood: sassy
+print(my_cat)  # deletes the key:value mood: sassy
